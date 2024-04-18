201c1f9603c1b4221891bb56ea6c9729f7e8d4d8
Lushito <luisdevcol@gmail.com>
2024-04-17 22:18:54 -0500

    instalacion de bootstrap 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 export const App = () => {
-
   return (
-    <h1>Latin tech</h1>
-  )
-}
-
-
+    <>
+      <h1>Latin tech</h1>
+      <button className="btn btn-primary">Click me</button>
+    </>
+  );
+};
